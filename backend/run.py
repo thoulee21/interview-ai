6b50b1c109e3b64d8138923680f569f20523d8ff
thoulee <thou_lee@outlook.com>
2025-05-05 14:08:08 +0800

    fix: 'FLASK_ENV' is deprecated and will not be used in Flask 2.3. Use 'FLASK_DEBUG' instead.

--- backend/run.py
+++ backend/run.py
@@ -4,10 +4,12 @@
 """
 
 import os
+
 from app import create_app
 
 # 确定配置环境
-env = os.environ.get('FLASK_ENV', 'development')
+flask_debug = os.environ.get('FLASK_DEBUG', '1')
+env = 'development' if flask_debug == '1' else 'production'
 
 # 创建应用实例
 app = create_app(env)
@@ -16,7 +18,7 @@ if __name__ == '__main__':
     # 获取主机和端口（如果环境变量中设置了）
     host = os.environ.get('HOST', '0.0.0.0')
     port = int(os.environ.get('PORT', 5000))
-    
+
     # 启动应用
     app.run(host=host, port=port, debug=(env == 'development'))
 
