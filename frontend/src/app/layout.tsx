27bab7ed41ab6d762eb1a3426986c0d4c148741c
thoulee <thou_lee@outlook.com>
2025-05-13 23:44:33 +0800

    feat(layout): 添加元数据和作者信息

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,9 +1,10 @@
+import pkg from "@/../package.json";
 import "@/app/index.css";
+import AppLayout from "@/components/layout/AppLayout";
 import "@ant-design/v5-patch-for-react-19";
 import Clarity from "@microsoft/clarity";
 import type { Metadata } from "next";
 import React from "react";
-import AppLayout from "@/components/layout/AppLayout";
 
 Clarity.init("rdpfluipqk");
 
@@ -14,6 +15,21 @@ export const metadata: Metadata = {
     icon: "/favicon.ico",
     shortcut: "/favicon.ico",
   },
+  authors: [pkg.author],
+  abstract: "中国软件杯参赛作品",
+  creator: pkg.author.name,
+  keywords: [
+    "中国软件杯",
+    "智能模拟面试系统",
+    "面试",
+    "模拟面试",
+    "AI",
+    "人工智能",
+    "面试助手",
+  ],
+  generator: "Next.js",
+  publisher: pkg.author.name,
+  applicationName: "智能模拟面试系统",
 };
 
 export default function RootLayout({
